649faa8bdd6c31d0c412e0f9869306fa9317dd25
Ben Jennings <ben@measured.com.au>
2014-04-08 09:53:10 +1000

    Add node-open

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp');
-
+var open = require('open');
 var EXPRESS_PORT = 4000;
 var EXPRESS_ROOT = __dirname;
 var LIVERELOAD_PORT = 35729;
 
 function startExpress() {
-
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')());
@@ -15,15 +14,12 @@ function startExpress() {
 
 var lr;
 function startLivereload() {
-
   lr = require('tiny-lr')();
   lr.listen(LIVERELOAD_PORT);
 }
 
 function notifyLivereload(event) {
-
   var fileName = require('path').relative(EXPRESS_ROOT, event.path);
-
   lr.changed({
     body: {
       files: [fileName]
@@ -32,8 +28,8 @@ function notifyLivereload(event) {
 }
 
 gulp.task('default', function () {
-
   startExpress();
   startLivereload();
   gulp.watch('*.js', notifyLivereload);
+  open('http://localhost:4000');
 });
